97d82447b662c66a263beb8e6d2375c1589c69e4
anandasaisoorisetty <anandasaisoorisetty@gmail.com>
2023-11-01 17:50:05 +0530

    Jenkinsfile updated

--- ecs-auto.sh
+++ ecs-auto.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-SERVICE="demo-java"
+SERVICE_NAME="demo-java"
 CLUSTER_NAME="demo-java"
 AWS_REGION="us-east-1"
 # export AWS_PROFILE=default
 
 # Register a new Task definition 
 aws ecs register-task-definition --family demo-new --cli-input-json file://task-new.json --region $AWS_REGION
+
+aws ecs create-service --cluster $CLUSTER_NAME --service-name $SERVICE_NAME --task-definition demo-new --desired-count 1 --region $AWS_REGION
 
 # Update Service in the Cluster
 # aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition demo-new --desired-count 1 --region $AWS_REGION
